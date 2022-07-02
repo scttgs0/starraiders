65589bf9d1d583b32166e01c7b7548f055950051
Scott Giese <scttgs0@gmail.com>
2022-07-02 03:09:40 -0500

    initial commit

---
+++ make_a8.sh
@@ -0,0 +1,16 @@
+64tass  --m65xx \
+        --atari-xex \
+        -o sraiders.xex \
+        -D MEDIA=0 \
+        --list=sraiders_a8_exe.lst \
+        --labels=sraiders_a8_exe.lbl \
+        sraiders.asm
+
+64tass  --m65xx \
+        --atari-xex \
+        -b \
+        -o sraiders.rom \
+        -D MEDIA=1 \
+        --list=sraiders_a8_rom.lst \
+        --labels=sraiders_a8_rom.lbl \
+        sraiders.asm
